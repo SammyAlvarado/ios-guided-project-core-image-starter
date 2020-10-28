c4ae7ab0ce219ba58512e8c44e846d85eaf14614
Sammy Alvarado <isammyalvarado@gmail.com>
2020-10-27 22:44:59 -0400

    implement core-image
    Implement the underpinning for core-image

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -10,30 +10,62 @@ class PhotoFilterViewController: UIViewController {
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
     
-    var originalImage: UIImage?
+    var originalImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    private let context = CIContext()
+    private let filter = CIFilter.colorControls()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
         
-        let filter = CIFilter.gaussianBlur()
-        let filter2 = CIFilter(name: "CIColorControls")
+//        let filter = CIFilter.gaussianBlur()
+//        let filter2 = CIFilter(name: "CIColorControls")
+//
+//        print(filter.attributes) // print the attributes to see what they can do.
         
-        print(filter.attributes) // print the attributes to see what they can do.
+        originalImage = imageView.image
 
 	}
-    
+
     private func presentImagePickerController() {
         guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
             print("This is were the error should be presented to the user NSLOG or Alert")
             return
         }
-        
+
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .photoLibrary
         imagePicker.delegate = self
         
         present(imagePicker, animated: true, completion: nil)
         
+    }
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        let inputImage = CIImage(image: image)
+        
+        filter.inputImage = inputImage
+        filter.saturation = saturationSlider.value
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        
+        guard let outputimage = filter.outputImage else { return image }
+        
+        guard let renderedCGImage = context.createCGImage(outputimage, from: outputimage.extent) else { return image}
+        
+        return UIImage(cgImage: renderedCGImage)
+    }
+    
+    private func updateImage() {
+        if let originalImage = originalImage {
+            imageView.image = originalImage
+        } else {
+            imageView.image = nil
+        }
     }
 	
 	// MARK: Actions
@@ -50,15 +82,15 @@ class PhotoFilterViewController: UIViewController {
 	// MARK: Slider events
 	
 	@IBAction func brightnessChanged(_ sender: UISlider) {
-
+        updateImage()
 	}
 	
 	@IBAction func contrastChanged(_ sender: Any) {
-
+        updateImage()
 	}
 	
 	@IBAction func saturationChanged(_ sender: Any) {
-
+        updateImage()
 	}
 }
 
